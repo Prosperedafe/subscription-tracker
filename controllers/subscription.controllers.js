efe2201b55def95c672cf630b2ac038eda3edcf5
Prosperedafe <mamusedafe19@gmail.com>
2026-03-05 15:36:10 +0100

    added getsubscription by id and delete subscription endpoints

--- controllers/subscription.controllers.js
+++ controllers/subscription.controllers.js
@@ -49,6 +49,56 @@ export const getUserSubscriptions = async (req, res, next) => {
     }
 }
 
+export const getUserSubscriptionById = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findById(req.params.id);
+        if (!subscription) {
+            const error = new Error("Subscription not found");
+            error.statusCode = 404;
+            throw error;
+        }
+        if (subscription.user.toString() !== req.user.id) {
+            const error = new Error("You are not the owner of this subscription");
+            error.statusCode = 401;
+            throw error;
+        }
+        res.status(200).json({
+            success: true,
+            message: "Subscription fetched successfully",
+            data: subscription
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const deleteSubscription = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findById(req.params.id);
+
+        if (!subscription) {
+            const error = new Error("Subscription not found");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        if (subscription.user.toString() !== req.user.id) {
+            const error = new Error("You are not the owner of this subscription");
+            error.statusCode = 401;
+            throw error;
+        }
+
+        await Subscription.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            success: true,
+            message: "Subscription deleted successfully"
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
 export const getSubscriptionList = async (req, res, next) => {
     try {
         const subscriptions = await SubscriptionList.find();
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import authorize from '../middlewares/auth.middleware.js';
-import { createSubscription, getUserSubscriptions, getSubscriptionList } from '../controllers/subscription.controllers.js';
+import { createSubscription, getUserSubscriptions, getSubscriptionList, getUserSubscriptionById, deleteSubscription } from '../controllers/subscription.controllers.js';
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.get('/list', authorize, getSubscriptionList);
 
 subscriptionRouter.post('/', authorize, createSubscription);
+subscriptionRouter.get('/:id', authorize, getUserSubscriptionById);
+
+subscriptionRouter.delete('/:id', authorize, deleteSubscription);
 
 subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 
